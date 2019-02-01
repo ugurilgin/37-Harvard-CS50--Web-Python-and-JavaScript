2cb35c0161bca24748e7092277b83dbbe2f9da50
ygautomo <y_gautomo@yahoo.com>
2019-02-01 23:32:01 +0700

    Add several files

--- lecture04-sql/notes/application.py
+++ lecture04-sql/notes/application.py
@@ -7,14 +7,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# notes = []
+notes = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	if session.get("notes") is None:
-		session["notes"] = []
-	if request.method == "POST":
-		note = request.form.get("note")
-		session["notes"].append(note)
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
 
-	return render_template("index.html", notes=session["notes"])
+    return render_template("index.html", notes=notes)
